92b9b3f66292facc64c5a6081525cde862fe131e
vlyagusha <vlyagusha@justclick.ru>
2021-05-15 14:06:32 +0300

    добавил кнопку выключения сервера

--- keyboards.py
+++ keyboards.py
@@ -4,9 +4,11 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemo
 
 button_warning = KeyboardButton(emoji.emojize('/say ⚠️ Осторожно! ⚠️ Посторонний! ⚠️'))
 button_danger = KeyboardButton(emoji.emojize('/say ‼️ 🔥 Опасность! 🔥 ‼️'))
+button_shutdown = KeyboardButton(emoji.emojize('/shutdown 🆘 Выключить сервер 🆘'))
 
 alert_kb = ReplyKeyboardMarkup(resize_keyboard=True)\
     .add(button_warning)\
-    .add(button_danger)
+    .add(button_danger)\
+    .add(button_shutdown)
 
 remove_kb = ReplyKeyboardRemove()
